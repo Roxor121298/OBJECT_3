f3c61bbf8a500aebc52e7d552c2827579de00849
Edouard Deschamps <edouarddeschamps@gmail.com>
2024-11-14 17:00:23 -0500

    annexe 9A

--- Annexe9/app/src/main/java/com/example/annexe9/MainActivity.java
+++ Annexe9/app/src/main/java/com/example/annexe9/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.annexe9;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView reponse1, reponse2, reponse3, reponse4, reponse5;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
 
 
         String url = "https://api.jsonbin.io/v3/b/6733b233ad19ca34f8c9149a?meta=false";
@@ -27,6 +45,66 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        reponse1 = findViewById(R.id.reponse1);
+        reponse2 = findViewById(R.id.reponse2);
+        reponse3 = findViewById(R.id.reponse3);
+        reponse4 = findViewById(R.id.reponse4);
+        reponse5 = findViewById(R.id.reponse5);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray jsonArray) {
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+
+                }
+            }) {
+           };
+
+
+
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        //pseudo code pour aider a complété
+                        JSONObject tab = null;
+                        try {
+                            Toast.makeText(getApplicationContext(), "Response: " + response.toString(), Toast.LENGTH_LONG).show();
+                            JSONObject le_premier = response.getJSONObject("menu");
+                            String le_deuxieme = le_premier.getString("header").toString();
+                            System.out.println("pass");
+                            reponse1.setText(le_deuxieme);
+
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        //Toast.makeText(getApplicationContext(), "Response: " + response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+
+        queue.add(jsonObjectRequest);
+
+
+
 
         
 
